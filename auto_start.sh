883ee661701aaaf709d9c29273d22658831ace45
RiSET <33034757+RiSET-NET@users.noreply.github.com>
2025-03-17 03:17:12 +0700

    Delete auto_start.sh

--- auto_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Add the miner to the startup script
-echo "Configuring auto-start for Termux..."
-echo "~/verus-miner/start_mining.sh" >> ~/.bashrc
-echo "Auto-start configuration complete. The miner will start when Termux is launched."
